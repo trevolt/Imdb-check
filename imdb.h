4dc9ad5170b7cfff516d470ea59ab4f00c967039
Sergey <s1392781@gmail.com>
2011-10-19 20:58:07 +0400

    Header file for program

--- imdb.h
+++ imdb.h
@@ -6,6 +6,8 @@
 #define str_s "Original Air Date"
 #define LENGTH_STRING_IN_FILE 500
 #define LENGTH_OF_FILENAME 50
+extern char* HOME;
+char config_file[LENGTH_OF_FILENAME];
 
 int copy_check(int k, char from[], char to[], int length, const char* str_search); //copy part of massive from from[] to to[] and then return position of first symbol of string searching str_search which has length length
 
@@ -18,5 +20,6 @@ int read_db(char name_of_object, const char* name_of_serial, char massive_to_cop
 void get_string(int pos, int pos1, char string[], char result_string[]); //this function copy massive from pos -> pos1 from massive string to massove result_string
 
 void print_usage(FILE* stream, int exid_code, const char* program_name); //prints usage information
+//void print_info(void);
 
 int get_episodes(char* DESTINATION_FILE, char* WEBPAGE_URL);
