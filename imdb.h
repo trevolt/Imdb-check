35b9de7d66802d6fdc4ffc1770546ae3da02bf5f
Sergey <s1392781@gmail.com>
2011-10-21 17:34:55 +0400

    Header file for program

--- imdb.h
+++ imdb.h
@@ -3,11 +3,15 @@
 #include <string.h>
 #include <getopt.h>
 #include <curl/curl.h>
+#include <sys/stat.h>
 #define str_s "Original Air Date"
 #define LENGTH_STRING_IN_FILE 500
 #define LENGTH_OF_FILENAME 50
-extern char* HOME;
-char config_file[LENGTH_OF_FILENAME];
+extern char* HOME; //variable for home directory
+char config_file[LENGTH_OF_FILENAME]; //our configuration file
+char config_directory[LENGTH_OF_FILENAME]; //our directory where all files will save
+char working_file[LENGTH_OF_FILENAME]; //our file with which we will work
+#define text_of_config "The big bang theory\\http://www.imdb.com/title/tt0898266/episodes\\ep_tbbt\\\nThe fringe\\http://www.imdb.com/title/tt1119644/episodes\\ep_fringe\\\nThe dexter\\http://www.imdb.com/title/tt0773262/episodes\\ep_dex\\\n" //this is example string in configuration file 
 
 int copy_check(int k, char from[], char to[], int length, const char* str_search); //copy part of massive from from[] to to[] and then return position of first symbol of string searching str_search which has length length
 
@@ -20,6 +24,5 @@ int read_db(char name_of_object, const char* name_of_serial, char massive_to_cop
 void get_string(int pos, int pos1, char string[], char result_string[]); //this function copy massive from pos -> pos1 from massive string to massove result_string
 
 void print_usage(FILE* stream, int exid_code, const char* program_name); //prints usage information
-//void print_info(void);
 
 int get_episodes(char* DESTINATION_FILE, char* WEBPAGE_URL);
